aa8dd0efdc6c5b9d7cc4fa96ee927328bf2c3805
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-08-31 13:46:33 -0300

    atualizar

--- js/displayFollowers.js
+++ js/displayFollowers.js
@@ -1,5 +1,17 @@
-const display = (followers) => {
-  console.log(followers)
-}
+const container = document.querySelector(".container");
 
-export default display
+const display = (followers) => {
+  //console.log(followers)
+  const newFollowers = followers.map((person) => {
+  const { avatar_url, login, html_url} = person
+    return `<article>
+    <img src="${avatar_url}" alt="${login} />
+    <h4>${login}</h4>
+    <a href="${html_url}" class="btn">view profile</a>
+    </article>`;
+  }).join('');
+  container.innerHTML = newFollowers
+  //console.log(newFollowers)
+};
+
+export default display;
